68438fa195a5d578ea35a916e3e4a026b4702f10
dilanbtk <2019141047@cumhuriyet.edu.tr>
2023-06-05 13:22:46 +0300

    new

---
+++ src/Context/WeatherContext.js
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const WeatherContext = createContext();
+
+export const WeatherProvider = ({ children }) => {
+  const [city, setCity] = useState({ lat: 37.8667, lon: 32.4833 });
+  const [weatherData, setWeatherData] = useState({});
+  const APIKEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+  /* Hava durumu bilgisini almak icin openweathermap API'na enlem, boylam ve apikey ile istekte bulunuyoruz
+    Her sehir degistiginde bu istek tekrar ediyor
+  */
+  useEffect(() => {
+    console.log( "SDFDHFHGH", APIKEY);
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${city.lat}&lon=${city.lon}&appid=${APIKEY}&units=metric&cnt=7`
+      )
+      .then((data) => {
+        setWeatherData(data.data);
+      });
+  }, [city, APIKEY]);
+
+  const values = {
+    weatherData,
+    setCity,
+  };
+
+  return (
+    <WeatherContext.Provider value={values}>{children}</WeatherContext.Provider>
+  );
+};
+
+export default WeatherContext;
